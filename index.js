e8f4afc0da9d76c49d1c01cbe3618e281282c939
markrestart <markrestart@gmail.com>
2024-03-27 21:40:28 -0400

    Prep as npm package

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-import PVR from "./lib/PVR.js";
+//This is the index file for the library. It exports the PVR class and the RenderNode class.
+
 import RenderNode from "./lib/RenderNode.js";
+import TextNode from "./lib/TextNode.js";
+import PVR from "./lib/PVR.js";
 
-let render = new PVR({
-  width: 800,
-  height: 600,
-  backgroundColor: "#000000",
-  duration: 10,
-  fps: 24,
-});
-
-let node = new RenderNode(50, 50, 50, 50);
-let animation = {
-  type: "linear",
-  start: 0,
-  end: 10,
-  from: { x: 50, y: 50 },
-  to: { x: 1000, y: 1000 },
-};
-node.addAnimation(animation);
-
-render.addChild(node);
-render.render();
+export default PVR;
+export { RenderNode, TextNode };
